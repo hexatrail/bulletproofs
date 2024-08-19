2bfeea1223b172e674b11761e9e173f3f9fae5f1
Hamza Saleem <104603197+hsaleemsupra@users.noreply.github.com>
2024-08-19 15:26:25 -0400

    adds support for n=7

--- src/range_proof/party.rs
+++ src/range_proof/party.rs
@@ -40,7 +40,7 @@ impl Party {
         v_blinding: Scalar,
         n: usize,
     ) -> Result<PartyAwaitingPosition<'a>, MPCError> {
-        if !(n == 8 || n == 16 || n == 32 || n == 64) {
+        if !(n == 7 || n == 8 || n == 16 || n == 32 || n == 64) {
             return Err(MPCError::InvalidBitsize);
         }
         if bp_gens.gens_capacity < n {
